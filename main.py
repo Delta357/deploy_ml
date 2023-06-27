771e56b1a4a88f1f574277b0732dc8ff5e8e2f1a
Gallo <rafaelhenriquegallo@gmail.com>
2023-06-27 11:28:19 -0300

    Update main.py
    ggh

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import pandas as pd
-from sklearn.ensemble import RandomForestClassifier
 import joblib
 
 # Carregar o modelo treinado e outros pré-processamentos necessários
